8066d4d535e319aa1d63e80867e45f4bf3bd0859
AlexDonley <iamdonley98@gmail.com>
2024-06-01 18:15:56 +0800

    progress bar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const texts = document.querySelector(".texts");
 const targetColumn = document.querySelector(".targetColumn");
-const title = document.querySelector("h1")
+const title = document.querySelector("h1");
+const progressParts = Array.from(document.getElementsByClassName('bar-part'));
 
 const chromeIcon = document.getElementById("chromeIcon");
 const mic = document.getElementById("mic");
@@ -46,6 +47,12 @@ let next = new Audio("sound/chaching.webm");
 let perfect = new Audio("sound/wow.mp3");
 
 score = 0;
+
+// order for progress bar data will be incomplete, leftover, complete
+progress = [0, 0, 0];
+totalWordsToRead = 0;
+updateProgressBar(progress);
+
 scoreMarker.innerText = score;
 sentenceCount = 0;
 
@@ -126,6 +133,10 @@ function startRound() {
     sentenceQueue = [];
     leftoversList = [];
 
+    totalWordsToRead = 0;
+    progress = [0, 0, 0];
+    
+
     if(!timerBool & timerSetting > 0){
       timerBool = true;
       
@@ -157,14 +168,28 @@ function startRound() {
         sentenceQueue = sentenceQueue.concat(bookList[num].text)
       })
 
+      sentenceQueue.forEach((str) => {
+        totalWordsToRead += str.split(' ').length;
+      })
+
       // console.log(sentenceQueue);
 
     } else {
-      let textArray = textInput.value.split('\n');
+      freeformText = textInput.value
+      
+      totalWordsToRead = freeformText.split(' ').length;
+      progress[0] = totalWordsToRead
+
+      console.log(progress)
+      
+      let textArray = freeformText.split('\n');
       // console.log(textArray);
       sentenceQueue = textArray;
     }
     
+    progress[0] = totalWordsToRead;
+    updateProgressBar(progress);
+
     if (shuffleBool) {
       sentenceQueue = shuffle(sentenceQueue)
     }
@@ -179,6 +204,25 @@ function startRound() {
     startMenu.classList.add('disappear');
 }
 
+function nextRound(){
+            
+  if (sentenceCount < sentenceQueue.length - 1) {
+    sentenceCount++
+    loadTarget(sentenceQueue[sentenceCount])
+  } else if (leftoversList.length > 0){
+    leftoversRound();
+  } else {
+    
+    updateAwards();              
+    
+    if(loopBool) {
+      startRound();
+    } else {
+      endRound();
+    }
+  }
+}
+
 function endRound() {
   listenBool = false;
   timerBool = false;
@@ -188,7 +232,7 @@ function endRound() {
   startMenu.classList.remove('disappear');
 }
 
-function loadTarget(sentence){
+function loadTarget(sentence, leftovers){
   targetColumn.innerHTML = '';
   texts.innerHTML = '';
 
@@ -205,11 +249,13 @@ function loadTarget(sentence){
     newSpan.setAttribute('id', ('target' + n));
     newSpan.classList.add('target');
 
-    leftButton = document.createElement('div');
-    leftButton.classList.add('leftButton');
-    leftButton.innerText = '◀';
-    newSpan.appendChild(leftButton);
-
+    if (!leftovers) {
+      leftButton = document.createElement('div');
+      leftButton.classList.add('leftButton');
+      leftButton.innerText = '◀';
+      newSpan.appendChild(leftButton);
+    }
+  
     text = (divided[n]).toLowerCase()
     newContent = document.createTextNode(text);
 
@@ -245,7 +291,11 @@ function updateSentenceVisual(num) {
   correctWord = targetList[num];
   correctWord.classList.add('correct')
 
-  correctWord.children[0].classList.add('remove-skip');
+  skipBtn = correctWord.children[0]
+
+  if (skipBtn) {
+    skipBtn.classList.add('remove-skip');
+  }
 
   if (num < divided.length - 1) {
     nextWord = targetList[num + 1];
@@ -275,13 +325,22 @@ function checkSentence(arr) {
         
         if (correct) {
           score++;
-          scoreMarker.innerText = score;;
-        
-        updateSentenceVisual(targetCount);
-        targetCount++;
+          scoreMarker.innerText = score;
 
-        let success = new Audio("sound/boop.wav");
-        success.play();
+          // update progress bar
+          progress[2]++;
+          if (progress[0] > 0) {
+            progress[0]--;
+          } else {
+            progress[1]--;
+          }
+          updateProgressBar(progress);
+        
+          updateSentenceVisual(targetCount);
+          targetCount++;
+
+          let success = new Audio("sound/boop.wav");
+          success.play();
         
         if (targetCount > divided.length - 1) {
           
@@ -306,25 +365,6 @@ function checkSentence(arr) {
 })
 }
 
-function nextRound(){
-            
-  if (sentenceCount < sentenceQueue.length - 1) {
-    sentenceCount++
-    loadTarget(sentenceQueue[sentenceCount])
-  } else if (leftoversList.length > 0){
-    leftoversRound();
-  } else {
-    
-    updateAwards();              
-    
-    if(loopBool) {
-      startRound();
-    } else {
-      endRound();
-    }
-  }
-}
-
 function leftoversRound() {
   let leftoversString = ""
   
@@ -335,7 +375,7 @@ function leftoversRound() {
     }
   }
   
-  loadTarget(leftoversString);
+  loadTarget(leftoversString, true);
   leftoversList = [];
 }
 
@@ -553,15 +593,22 @@ function flip(element) {
 
 function moveToleftoversList (n) {
   leftoversList.push(divided[n]);
+  console.log('pushed item to leftovers')
   divided.splice(n, 1);
 
   deleteSpan = targetList[n];
   deleteSpan.remove();
 
   updateTargetOrder();
-  //console.log(leftoversList);
+  
+  // update progress bar
+  progress[1]++;
+  progress[0]--;
 
-  if (n > targetList.length - 1){
+  updateProgressBar(progress)
+
+  console.log (targetCount);
+  if (n > targetList.length - 1 && targetCount == n){
     nextRound();
   }
 }
@@ -582,9 +629,21 @@ document.addEventListener('click', function(e) {
 
     for (let n = 0; n < targetList.length; n++){
       if (targetList[n].id == currentID) {
-        moveToleftoversList(n);
+        setTimeout(function() {moveToleftoversList(n)}, 5);
       }
     }
   }
        
 }, false);
+
+function updateProgressBar(arr) {
+  let incomplete = 100 * arr[0] / totalWordsToRead;
+  let leftover = 100 * arr[1] / totalWordsToRead;
+  let complete = 100 * arr[2] / totalWordsToRead;
+
+  console.log(incomplete, leftover, complete)
+
+  progressParts[0].style.height = incomplete + "%";
+  progressParts[1].style.height = leftover + "%";
+  progressParts[2].style.height = complete + "%";
+}
