1a1067f2fe678bb7c344bf6d230cd6dd22177eb2
Alex Donley <152264753+AlexDonley@users.noreply.github.com>
2025-02-13 16:41:53 +0800

    Update oscillate.js

--- js/oscillate.js
+++ js/oscillate.js
@@ -2,13 +2,16 @@
 
 const context = new AudioContext()
 
+const majorChord = [261.62, 329.62, 391.99, 523.33]
+const minorChord = [261.62, 311.12, 391.99, 523.33]
+
 export function oscBeep(freq, vol, length, type) {
     console.log('starting beep')
 
     const oscillator = context.createOscillator()
 
     // type can be triangle, sine, square, or sawtooth
-    oscillator.type = "square"
+    oscillator.type = type
     oscillator.frequency.value = freq
     
     const gainNode = context.createGain()
@@ -19,4 +22,22 @@ export function oscBeep(freq, vol, length, type) {
     oscillator.connect(gainNode)
     gainNode.connect(context.destination)
     oscillator.start()
+}
+
+export function createChord(arr, length, delay) {
+    
+    let thisChord = majorChord
+
+    if (arr) {
+        thisChord = arr
+    }
+
+    let n = 0;
+    thisChord.forEach(val => {
+        setTimeout(() => {
+            oscBeep(val, 0.04, length, 'sawtooth')
+        }, n * delay)
+
+        n++
+    })
 }
